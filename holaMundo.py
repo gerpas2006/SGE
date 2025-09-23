724a17891ea0c6fc0b9505e877519fcbd8c90a82
Germán <german.pastor2006@gmail.com>
2025-09-23 08:57:35 +0200

    subida ejemplo

--- holaMundo.py
+++ holaMundo.py
@@ -1,5 +1,10 @@
 msg  = "Hello world"
+num1 =123
+pi = 3.14
 print(msg)
 a = input("Introduce un número ")
 b = input("Introduce otro número ")
 print("La suma de los dos número es:", int(a) + int(b))
+print(num1)
+print(pi)
+
