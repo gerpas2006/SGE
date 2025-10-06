4b7a950712c57ba7e75e76c003e003285695318e
web <german.pastor2006@gmail.com>
2025-10-06 16:42:01 +0200

    subida ej12

---
+++ ejercicioT01/ejercicio12.py
@@ -0,0 +1,13 @@
+import random;
+numAleatorio=int(random.randint(0,2))
+numElegido=int(0)
+contador=int(1)
+while numElegido==numAleatorio or contador<=3:
+    numElegido=int(input("Dime el numero que crees que ha salido "))
+    if(numAleatorio==numElegido):
+        print("Has adivinado el numero")
+        break
+    contador+=1
+print(f"El número generado era el {numAleatorio}")
+
+
