b90919c13a2720c3ff8d00a2efdf42fe74119d79
Germán <german.pastor2006@gmail.com>
2025-10-06 08:57:26 +0200

    subida

---
+++ ejemplosT01/ejermploBucles.py
@@ -0,0 +1,10 @@
+want_greet="s"
+while want_greet == "s":
+    print("Hola que tal")
+    want_greet= input("¿Quiere otro saludo? [s/n] ")
+print("Que tenga un buen dia")
+
+
+word="Python"
+for letter in word:
+    print(letter)
