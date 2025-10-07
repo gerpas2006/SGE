b13aeb65abb1cb58dc9bc37b756833d334336995
Germán <german.pastor2006@gmail.com>
2025-10-07 11:51:59 +0200

    subida

--- ejercicioT01/ejercicio11.py
+++ ejercicioT01/ejercicio11.py
@@ -1,13 +1,14 @@
-objetivo = int(input("Introduce un número: "))
-list = []
-suma=0
-while suma != objetivo:
-    num= int(input("Dame un número"))
-    list.append(num)
-    suma+=num
-    if suma>objetivo:
-        print("La suma de los numeros que has puesto se pasan ya del objetivo. Has perdidoooo!!!!")
-        suma=objetivo
+objetivo = int(input("Introduce un número: ")) 
+list = [] 
+suma=0 
+while suma != objetivo: 
+        num= int(input("Dame un número ")) 
+        suma+=num 
+        if suma>objetivo: 
+            print("La suma de los numeros que has puesto se pasan ya del objetivo.Te voy a restar el último numero que has puesto") 
+            suma-=num 
+        else:
+            list.append(num) 
+
 
 print(f"Estos han sido todos los número que has dicho {list}")
-
