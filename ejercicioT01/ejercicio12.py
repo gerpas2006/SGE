54d4c0d2c555cfb23d19d3db83ed2d171cae74e6
Germán <german.pastor2006@gmail.com>
2025-10-09 09:00:23 +0200

    subida correcciones

--- ejercicioT01/ejercicio12.py
+++ ejercicioT01/ejercicio12.py
@@ -1,10 +1,10 @@
 import random;
-numAleatorio=int(random.randint(0,2))
+numAleatorio=int(random.randint(0,10))
 numElegido=int(0)
 contador=int(1)
 while numElegido==numAleatorio or contador<=3:
     numElegido=int(input("Dime el numero que crees que ha salido "))
-    if(numAleatorio==numElegido):
+    if numAleatorio==numElegido:
         print("Has adivinado el numero")
         break
     contador+=1
---
+++ ejercicioT01/ejercicio14.py
@@ -0,0 +1,5 @@
+triangulo = int(input("Dime el numero de veces"))
+for i in range(0,triangulo):
+    print("*" *i)
+for j in range(triangulo,0,-1):
+    print("*" *j)
