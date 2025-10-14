1bf27b7ac94aa7b455583761708e96d4bd399cee
Germán <german.pastor2006@gmail.com>
2025-10-14 08:57:25 +0200

    subida

---
+++ ejemplosT2/ejemplos01.py
@@ -0,0 +1,11 @@
+empty_list = []
+languajes = ["Python", "Ruby" , "JvaScript"]
+data = ["Tenerife", {"cielo" : "limpio", "temp": 24},3718,(28.2933947,-16.5226597)]
+ciudades = ["Sevilla","Madrid","Barcelona", "Málaga", "Cáceres"]
+print(list("Python"))
+print(ciudades[1])
+print(ciudades[-1])
+print(ciudades[4])
+print(ciudades[0:3])
+print(ciudades[:3])
+print(ciudades[-1:-4:-1])
