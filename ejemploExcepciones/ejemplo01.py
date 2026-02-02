1c25cb76a8192366cede1fde1d1964ac9a6bbcf6
Germán <german.pastor2006@gmail.com>
2026-02-02 08:55:01 +0100

    subida

---
+++ ejemploExcepciones/ejemplo01.py
@@ -0,0 +1,34 @@
+def intdiv(a:int,b:int) -> int:
+    try:
+        return a // b
+    except TypeError:
+        print("Operador incorrecto")
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+    except Exception:
+        print("Error")
+
+
+intdiv(3,0)
+intdiv(3,'0')
+
+def intdiv3(a:int,b:int) -> int:
+    try:
+        return a // b
+    except (TypeError,ZeroDivisionError):
+        print("Comprueba el operando, hay mas errores")
+    except Exception:
+        print("Error")
+
+
+intdiv3(7,0)
+
+values = [4,2,7]
+try:
+    r = values[3]
+except IndexError:
+    print("Error,el indice señaldo no esta")
+else:
+    print(f"El número buscado es {r}")
+finally:
+    print("Que tengas un buen día")
